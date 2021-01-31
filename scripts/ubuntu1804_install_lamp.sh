9d9cc597da3afa7244cfefd2183e1bae3056e52b
eric010101 <43527995+eric010101@users.noreply.github.com>
2021-01-31 23:35:08 +0800

    Update ubuntu1804_install_lamp.sh

--- scripts/ubuntu1804_install_lamp.sh
+++ scripts/ubuntu1804_install_lamp.sh
@@ -287,24 +287,6 @@ sed -i "s/display_errors = Off/display_errors = On/g" /etc/php/7.4/apache2/php.i
 sudo systemctl restart apache2
 
 #################################################
-# upgrade to php7.4
-#################################################
-cd
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt update -y
-sudo apt install php7.4 libapache2-mod-php7.4 -y
-sudo a2dismod php7.2
-sudo a2enmod php7.4
-sudo systemctl restart apache2
-sudo apt install -y libapache2-mod-php7.4 libapache2-mod-php7.4 php7.4-cli php7.4-mysql php7.4-gd php7.4-dev php7.4-curl php7.4-opcache
-sudo apt install -y php7.4-common php7.4-xml php7.4-xmlrpc php7.4-imagick php7.4-dev php7.4-imap php7.4-mbstring php7.4-soap php7.4-zip php7.4-intl
-sudo systemctl restart apache2
-sed -i "s/display_errors = Off/display_errors = On/g" /etc/php/7.4/apache2/php.ini
-
-sudo systemctl restart apache2
-
-#################################################
 # install ipfs
 #################################################
 cd
