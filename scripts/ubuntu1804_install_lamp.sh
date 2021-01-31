0922db0713162622bac5f0dc3ec0f75e70858817
eric010101 <43527995+eric010101@users.noreply.github.com>
2021-01-31 21:22:07 +0800

    Update ubuntu1804_install_lamp.sh

--- scripts/ubuntu1804_install_lamp.sh
+++ scripts/ubuntu1804_install_lamp.sh
@@ -342,7 +342,7 @@ sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 # install ipfs-php-api
 #################################################
 sudo mkdir /var/www/html/ipfs-php
-#cd /var/www/html/ipfs-php
+cd /var/www/html/ipfs-php
 sudo chmod -R 0777 /var/www/html/ipfs-php/.
 composer require rannmann/php-ipfs-api dev-master -d /var/www/html/ipfs-php
 #sudo composer install
