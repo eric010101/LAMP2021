1bf29ca4014f5f704793b5e8592133392d17043b
eric010101 <43527995+eric010101@users.noreply.github.com>
2021-01-31 22:00:48 +0800

    Create composer-ipfs-php.pl
    expect composer

---
+++ composer-ipfs-php.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/expect -f
+set force_conservative 0  ;# set to 1 to force conservative mode even if
+			  ;# script wasn't run conservatively originally
+if {$force_conservative} {
+	set send_slow {1 .1}
+	proc send {ignore arg} {
+		sleep .1
+		exp_send -s -- $arg
+	}
+}
+set timeout -1
+spawn composer require rannmann/php-ipfs-api dev-master -d /var/www/html/ipfs-php
+match_max 100000
+expect "Continue as root/super user [yes]?"
+send -- "yes\r"
+expect eof
